43fbb66810d7cdab76d0d0feffd42275b494bd8e
Jesmaster <jesmasterha@gmail.com>
2020-04-04 16:55:41 -0400

    Created Board Game Geek game search bot

---
+++ commands/bgg-search.js
@@ -0,0 +1,55 @@
+module.exports = {
+    name: 'bgg-search',
+    description: 'Search Boardgamegeek for game info. Args: {game_name}',
+    usage: '<game_name>',
+    args: true,
+    execute: async function(message, args) {
+        const Discord = require('discord.js');
+        const fetch = require('node-fetch');
+        const querystring = require('querystring');
+        const xml2js = require('xml2js');
+
+        const query = querystring.stringify({ query: args.join(' ') });
+        //const inspect = require('eyes').inspector({maxLength: false});
+
+        fetch(`https://boardgamegeek.com/xmlapi2/search?type=boardgame&${query}`).then(response => {
+            response.text().then(xml => {
+                xml2js.parseString(xml, function (err, result) {
+                    if(parseInt(result.items['$'].total, 10) === 0){
+                        message.channel.send(`No results found for "${args.join(' ')}".`);
+                    }
+                    else {
+                        const thing_id = result.items.item[0]['$'].id;
+                        fetch(`https://boardgamegeek.com/xmlapi2/thing?type=boardgame,boardgameexpansion&id=${thing_id}`).then(response => {
+                            response.text().then(xml => {
+                                xml2js.parseString(xml, function(err, result) {
+                                    const item = result.items.item[0];
+                                    const embed = new Discord.MessageEmbed()
+                                        .setColor('#3f3a60')
+                                        .setTitle(item.name[0]['$'].value)
+                                        .setURL(`https://boardgamegeek.com/${item['$'].type}/${item['$'].id}`)
+                                        .setThumbnail(item.thumbnail[0])
+                                        .setDescription(item.description[0].substr(0, 200)+'...')
+                                        .addFields(
+                                            {
+                                                name: 'Number of Players',
+                                                value: `${item.minplayers[0]['$'].value} - ${item.maxplayers[0]['$'].value}`,
+                                                inline: true
+                                            },
+                                            {
+                                                name: 'Average Playtime',
+                                                value: `${item.playingtime[0]['$'].value} min`,
+                                                inline: true
+                                            }
+                                        );
+
+                                    message.channel.send(embed)
+                                });
+                            });
+                        });
+                    }
+                });
+            });
+        });
+    },
+};
---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+
+    const command = client.commands.get(commandName);
+
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+client.login(token);
