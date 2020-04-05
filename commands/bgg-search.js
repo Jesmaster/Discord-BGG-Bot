6dfc30afe3bb97f7ab6af6bbdc11ef9b4b98bc8a
Jesmaster <jesmasterha@gmail.com>
2020-04-04 21:35:42 -0400

    More refactoring and cleanup. Updated search Fixes #4

--- commands/bgg-search.js
+++ commands/bgg-search.js
@@ -3,15 +3,8 @@ module.exports = {
     description: 'Search Boardgamegeek for game info. Args: {game_name}',
     usage: '<game_name>',
     args: true,
-    parseXml: function(xml) {
-        const xml2js = require('xml2js');
-
-        xml2js.parseString(xml, function (err, result) {
-            return result;
-        });
-
-        return false;
-    },
+    api_url : 'https://boardgamegeek.com',
+    types: ['boardgame', 'boardgameexpansion'],
     /**
      * Preforms BGG API search call.
      * First attempt exact name match call. If no results then attempt partial name match.
@@ -19,32 +12,55 @@ module.exports = {
      * @param {Array} args
      * @param {Boolean} exact
      *
-     * @return {Promise<string>}
+     * @return {Promise<JSON>}
      */
     bggSearch: async function(args, exact = false) {
         const fetch = require('node-fetch');
         const querystring = require('querystring');
+        const xml2js = require('xml2js');
 
-        const query = querystring.stringify({ query: args.join(' ') });
+        const query = querystring.stringify({
+            type: this.types.join(','),
+            query: args.join(' ')
+        });
 
-        let url = `https://boardgamegeek.com/xmlapi2/search?type=boardgame&${query}`;
+        let url = `${this.api_url}/xmlapi2/search?${query}`;
         if(exact) {
             url += '&exact=1';
         }
 
-        try {
-            fetch(url)
-                .then(response => response.text()
-                    .then(xml => { return this.parseXml(xml) })
-                );
-        } catch(error) {}
+        return fetch(url)
+            .then(response => response.text()
+                .then(xml => xml2js
+                    .parseStringPromise(xml)
+                    .then(result => result)
+                    .catch(err => { throw err })
+                ));
     },
+    /**
+     * Preforms BGG API thing call.
+     *
+     * @param {String} thing_id
+     *
+     * @return {Promise<JSON>}
+     */
     bggThing: async function(thing_id) {
         const fetch = require('node-fetch');
+        const querystring = require('querystring');
+        const xml2js = require('xml2js');
 
-        fetch(`https://boardgamegeek.com/xmlapi2/thing?type=boardgame,boardgameexpansion&id=${thing_id}`)
+        const query = querystring.stringify({
+            type: this.types.join(','),
+            id: thing_id
+        });
+
+        return fetch(`${this.api_url}/xmlapi2/thing?${query}`)
             .then(response => response.text()
-                .then(xml => { return this.parseXml(xml) }));
+                .then(xml => xml2js
+                    .parseStringPromise(xml)
+                    .then(result => result)
+                    .catch(err => { throw err })
+                ));
     },
     itemToEmbed: function(item) {
         const Discord = require('discord.js');
@@ -70,15 +86,24 @@ module.exports = {
             );
     },
     execute: async function(message, args) {
-        const inspect = require('eyes').inspector({maxLength: false});
+        //const inspect = require('eyes').inspector({maxLength: false});
 
         this.bggSearch(args, true).then(
             result => {
-                let found = parseInt(result.items['$'].total, 10) !== 0;
+                let found =
+                    parseInt(result.items['$'].total, 10) !== 0 &&
+                    result.items.item[result.items.item.length - 1].name[0]['$'].type === 'primary';
+
+                let thing_id = '';
+
+                //If exact search finds results, return the last (newest game) result
+                if(found) {
+                    thing_id = result.items.item[result.items.item.length - 1]['$'].id ;
+                }
 
                 return {
                     found: found,
-                    thing_id: found ? result.items.item[0]['$'].id : '',
+                    thing_id: thing_id,
                 };
             }
         ).then(
@@ -87,23 +112,38 @@ module.exports = {
                     return bggSearchResult.thing_id;
                 }
                 else {
+                    //If exact search fails, run word search instead. Return first result.
                     this.bggSearch(args).then(
                         result => {
                             if(parseInt(result.items['$'].total, 10) !== 0) {
                                 return result.items.item[0]['$'].id;
                             }
+                            else {
+                                message.channel.send(`No results found for "${args.join(' ')}".`);
+                                return '';
+                            }
                         }
-                    )
-                }
+                    ).then(
+                        thing_id => {
+                            if(thing_id !== '') {
+                                this.bggThing(thing_id)
+                                    .then(result => {
+                                        message.channel.send(this.itemToEmbed(result.items.item[0]));
+                                    });
+                            }
+                        });
 
-                message.channel.send(`No results found for "${args.join(' ')}".`);
-                return '';
+                    return '';
+                }
             }
-        ).then(thing_id => {
-            this.bggThing(thing_id)
-                .then(result => {
-                    message.channel.send(this.itemToEmbed(result.items.item[0]));
-                });
+        ).then(
+            thing_id => {
+            if(thing_id !== '') {
+                this.bggThing(thing_id)
+                    .then(result => {
+                        message.channel.send(this.itemToEmbed(result.items.item[0]));
+                    });
+            }
         });
     },
 };
