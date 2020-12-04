79be44e8a06fac40877d6515d905ed9c1bf2b67b
Jesmaster <jesmasterha@gmail.com>
2020-12-03 19:58:20 -0500

    Added server count to help command output

--- commands/bgg-help.js
+++ commands/bgg-help.js
@@ -8,13 +8,17 @@ module.exports = {
      *
      * @return {module:"discord.js".MessageEmbed}
      */
-    helpEmbed: async function() {
+    helpEmbed: async function(message) {
         const Discord = require('discord.js');
 
         return new Discord.MessageEmbed()
             .setColor('#3f3a60')
             .setTitle('BGG Bot Commands')
             .addFields(
+                {
+                  name: 'Stats',
+                  value: 'BGG Bot is on '+message.client.guilds.cache.size+' servers'
+                },
                 {
                     name: 'Collection',
                     value: 'Get collection information for a bgg user.\nUsage: `!bgg collection <username>`.\nExample: `!bgg collection jesmaster`'
@@ -33,7 +37,7 @@ module.exports = {
      * @return {Promise<void>}
      */
     execute: async function(message, args) {
-        this.helpEmbed().then(embed => {
+        this.helpEmbed(message).then(embed => {
             message.delete();
             message.channel.send(embed);
         });
