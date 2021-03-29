041a0ced00dc5b0aa114006e534c8dcf014774ab
Jesmaster <jesmasterha@gmail.com>
2021-03-29 19:29:30 -0400

    Prefix cache keys with cache type

--- commands/bgg-collection.js
+++ commands/bgg-collection.js
@@ -20,6 +20,7 @@ module.exports = {
             console.log('Connection Error', err);
         });
 
+        cache_key = cache_type + '_' + cache_key;
         let cache = await keyv.get(cache_key);
 
         keyv.opts.store.redis.disconnect();
@@ -42,6 +43,7 @@ module.exports = {
             Keyv = require('keyv'),
             keyv = new Keyv(process.env.REDIS_URL);
 
+        cache_key = cache_type + '_' + cache_key;
         await keyv.set(cache_key, cache_data, this.cache_ttl);
 
         keyv.opts.store.redis.disconnect();
