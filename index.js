c5b12e842ed0020e4bb435f58cd391ccb752d4b9
Jesmaster <jesmasterha@gmail.com>
2022-05-07 20:38:33 -0400

    Fixed token variable

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const
     fs = require('fs'),
-    { token } = process.env.TOKEN,
+    token = process.env.TOKEN,
     client = new Client({
         intents: [
             Intents.FLAGS.GUILDS,
